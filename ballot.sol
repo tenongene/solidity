ed7151db24ead9e3a19196768d5fd8d7d7ebb0f8
Tenongene <tenongene00@gmail.com>
2023-03-23 19:46:46 -0500

    Initial commit

---
+++ ballot.sol
@@ -0,0 +1,16 @@
+// SPDX
+pragma solidity ^0.8.19;
+
+contract Inbox {
+  string public message;
+
+  constructor (string memory initialMessage) public {
+    message = initialMessage;
+  }
+  function setMessage(string memory newMessage) public {
+    message = newMessage;
+  }
+  function getMessage() public view returns (string memory) {
+        return message;
+  }
+}
